2c0d2de2c52020e1b36ac548534c2e217c0d2d25
ermeng <2444622785@qq.com>
2024-06-04 21:03:15 +0800

    5

---
+++ imkernel/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+ import Demo from './views/01/Demo.vue'
+</script>
+
+<template>
+    <div>
+        <Demo></Demo>
+    </div>
+</template>
+
+<style lang="css">
+/* div {
+    font-size: 50px;
+    color: red;
+} */
+</style>
---
+++ imkernel/src/views/01/Demo.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+console.log('count :>> ', count.value);
+const handleClick = () => {
+    count.value++
+}
+</script>
+
+<template>
+    <div>
+        {{ count }}
+
+        <button @click="handleClick">add</button>
+    </div>
+</template>
+
+<style lang="css"></style>
